b8c518156adeccc0be24845c7db85f45a67f22ab
Denis Halitsky <koldyr@users.noreply.github.com>
2021-10-06 22:42:56 +0300

    Orika mapper

--- model/src/main/resources/sql/initial-data.sql
+++ model/src/main/resources/sql/initial-data.sql
@@ -2,7 +2,7 @@ insert into T_AUTHOR (AUTHOR_ID, FIRST_NAME, LAST_NAME, DATE_OF_BIRTH)
 values (SEQ_AUTHOR.nextval, 'a1_first', 'a1_last', '1990-10-10');
 
 insert into T_BOOK (BOOK_ID, TITLE, PUBLISHING_HOUSE, AUTHOR_ID, PUBLICATION_DATE, GENRE, BOOK_COVER, NOTE)
-values (SEQ_BOOK.nextval, 'b1_title', 'b1_house', SEQ_AUTHOR.currval, '2021-09-09', 'SCI-FI', null, 'b1_note');
+values (SEQ_BOOK.nextval, 'b1_title', 'b1_house', SEQ_AUTHOR.currval, '2021-09-09', 'SCIFI', null, 'b1_note');
 
 insert into T_READER (READER_ID, FIRST_NAME, LAST_NAME, MAIL, ADDRESS, PHONE_NUMBER, NOTE)
 values (SEQ_READER.nextval, 'r1_first', 'r1_last', 'r1_mail', 'r1_address', 'r1-111-222-333', 'r1_note');
